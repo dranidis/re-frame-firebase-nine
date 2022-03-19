7631ad2b5648f71d280f7ccff1fb9af257855958
D Dranidis <dranidis@gmail.com>
2022-03-19 21:47:04 +0200

    Clean

--- src/re_frame_firebase_nine/example/emulator.cljs
+++ src/re_frame_firebase_nine/example/emulator.cljs
@@ -1,8 +1,4 @@
-(ns re-frame-firebase-nine.example.emulator
-  (:require ["firebase/database" :as fdb]
-            ["firebase/auth" :as fauth]
-            [re-frame-firebase-nine.firebase-database :refer [set-value! get-db]]
-            [re-frame-firebase-nine.firebase-auth :refer [get-auth]]))
+(ns re-frame-firebase-nine.example.emulator)
 
 (defn get-config
   []
@@ -13,13 +9,3 @@
    :storageBucket "re-frame-firebase-nine-example.appspot.com",
    :messagingSenderId "1052132575682",
    :appId "1:1052132575682:web:a0d28b5405ab1825e8ee5b"})
-
-
-(comment
-
-  (.-hostname js/location)
-
-  (set-value! ["users"] "test-value"
-              #(println "SUCCESS") #(println "ERROR" (js->clj %)))
- ;
-  )
